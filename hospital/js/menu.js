192477fc5d87f974972b86378b0981e5267dbb2a
Mhjinee <149984605+Mhjinee@users.noreply.github.com>
2024-09-08 17:46:35 +0900

    Delete hospital directory

--- hospital/js/menu.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-    $('#gnb>li>a').mouseenter(function(){
-        $(this).next('div').show();
-    });
-    $('#gnb>li').mouseleave(function(){
-        $('.gnb_set').hide();
-    });
-});
