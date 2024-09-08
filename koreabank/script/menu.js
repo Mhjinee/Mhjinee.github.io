c3263f302531b23179f4c480d3acd857e5a83442
Mhjinee <149984605+Mhjinee@users.noreply.github.com>
2024-09-08 17:49:45 +0900

    Delete koreabank directory

--- koreabank/script/menu.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-    $('.gnb>ul>li>a').mouseenter(function(){
-        $(this).next('div').show();
-    });
-    $('.gnb>ul>li').mouseleave(function(){
-        $('.subset').hide();
-    });
-});
